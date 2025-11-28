54056b96533914b19286974e3775b28d99103949
Bindushree <bindushreem06@gmail.com>
2025-11-28 16:17:22 +0530

    Initial commit

---
+++ src/main/java/org/example/DAO/EmployeeNotFoundException.java
@@ -0,0 +1,10 @@
+package org.example.DAO;
+
+
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
